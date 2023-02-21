791984a139c3b54fea999d3496ca701c496a02c2
crow02531 <2192749414@qq.com>
2023-02-21 18:06:14 +0800

    update

--- assets/js/splash.js
+++ assets/js/splash.js
@@ -1,13 +1,13 @@
 class Splash {
 
+  static #SPLASH_LIST = ["HIIIIIIISSS!", "Grrrrr...", "Full of stars!", "Ow!", "0% sugar!", "Yiiiiike!", "Eek!", "Haha, LOL!", "Woah.", "Whoops!", "Random splash!", "Khaaaaaaaaan!", "Ride the pig!", "Yaaay!", "90% bug free!", "Wow!", "Pumpkinhead!"]
+
   /**
    * Randomly pick up a splash.
    * 
    * @returns {string}
    */
   static pickSplash() {
-    return Splash.splashList[Math.floor(Math.random() * Splash.splashList.length)];
+    return Splash.#SPLASH_LIST[Math.floor(Math.random() * Splash.#SPLASH_LIST.length)]
   }
 }
-
-Splash.splashList = ["HIIIIIIISSS!", "Grrrrr...", "Full of stars!", "Ow!", "0% sugar!", "Yiiiiike!", "Eek!", "Haha, LOL!", "Woah.", "Whoops!", "Random splash!", "Khaaaaaaaaan!", "Ride the pig!", "Yaaay!", "90% bug free!", "Wow!", "Pumpkinhead!"];
